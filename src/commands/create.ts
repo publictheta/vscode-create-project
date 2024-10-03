6d48432951b9e487ca480dc31211deef44098e33
Public Theta <publictheta@gmail.com>
2024-10-03 22:09:17 +0900

    chore: refactor create command

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -14,6 +14,8 @@ function generateRandomName() {
 }
 
 export async function create() {
+    // Check the file system
+
     if (!vscode.workspace.fs.isWritableFileSystem("file")) {
         await vscode.window.showErrorMessage(
             vscode.l10n.t("This file system is not writable."),
@@ -21,6 +23,8 @@ export async function create() {
 
         return
     }
+
+    // Get configuration
 
     const defaultFolder = vscode.workspace
         .getConfiguration("create-project")
@@ -30,23 +34,40 @@ export async function create() {
         .getConfiguration("create-project")
         .get<string>("defaultName", "{randomName}")
 
-    const name = format(defaultName, { randomName: generateRandomName })
+    // Check `defaultFolder`
 
-    let value: string
+    let defaultFolderUri: vscode.Uri
 
     try {
-        value = vscode.Uri.joinPath(
-            vscode.Uri.file(defaultFolder),
-            name,
-        ).fsPath.toString()
+        defaultFolderUri = vscode.Uri.file(defaultFolder)
     } catch {
         await vscode.window.showErrorMessage(
             vscode.l10n.t(
-                "Failed to parse the default folder path. Please check the extension settings.",
+                "Failed to parse the default folder: {0}",
+                defaultFolder,
             ),
         )
         return
     }
+
+    // Format `defaultName`
+
+    const name = format(defaultName, { randomName: generateRandomName })
+
+    // Check `defaultName`
+
+    let value: string
+
+    try {
+        value = vscode.Uri.joinPath(defaultFolderUri, name).path
+    } catch {
+        await vscode.window.showErrorMessage(
+            vscode.l10n.t("Failed to parse the default name: {0}", defaultName),
+        )
+        return
+    }
+
+    // Ask the project folder path
 
     const input = await vscode.window.showInputBox({
         title: vscode.l10n.t("Input your project folder path."),
@@ -62,30 +83,36 @@ export async function create() {
         return
     }
 
+    // Parse the project folder path
+
     let uri: vscode.Uri
 
     try {
         uri = vscode.Uri.file(input)
     } catch {
         await vscode.window.showErrorMessage(
-            vscode.l10n.t("Failed to parse the folder path: {0}", input),
-        )
-
-        return
-    }
-
-    try {
-        await vscode.workspace.fs.createDirectory(uri)
-    } catch {
-        await vscode.window.showErrorMessage(
             vscode.l10n.t(
-                "Failed to create the directory: {0}",
-                uri.toString(),
+                "Failed to parse the project folder path: {0}",
+                input,
             ),
         )
 
         return
     }
+
+    // Create the directory
+
+    try {
+        await vscode.workspace.fs.createDirectory(uri)
+    } catch {
+        await vscode.window.showErrorMessage(
+            vscode.l10n.t("Failed to create the directory: {0}", uri.path),
+        )
+
+        return
+    }
+
+    // Open the directory
 
     await vscode.commands.executeCommand("vscode.openFolder", uri)
 }
